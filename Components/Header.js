5166091761acbf2393866186dd27b5c00955b909
Diya-Mariam <diyamariambiju25@gmail.com>
2022-02-06 22:05:07 +0530

    first commit

---
+++ Components/Header.js
@@ -0,0 +1,19 @@
+import headerStyle from '../styles/Header.module.css'
+
+const Header = () => {
+
+    return(
+
+        <div>
+
+            <h1 className= {headerStyle.title}>
+                <span>WebDev </span> News
+            </h1>
+            <p className={headerStyle.description}>Keep up to date with latest web dev news</p>
+
+        </div>
+
+    )
+}
+
+export default Header
